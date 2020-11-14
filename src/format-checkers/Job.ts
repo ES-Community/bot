e7ace61d02b06484a868d44d7a42649481a61b67
Xavier Stouder <xavier@stouder.io>
2020-11-14 02:05:59 +0100

    chore: check job format with a function

--- src/format-checkers/Job.ts
+++ src/format-checkers/Job.ts
@@ -1,11 +1,20 @@
 import FormatChecker from '../framework/FormatChecker';
 
-const jobRegexp = '^\\*\\*(\\[ ([^\\]])* \\] - ){2}[a-zA-Z ]+\\*\\*\n(.*\n?)*$';
-
 export default new FormatChecker({
   enabled: true,
   name: 'Job',
   description: 'Force le formattage du channel #jobs.',
   channelName: 'jobs',
-  checker: new RegExp(jobRegexp),
+  checker: (message) => {
+    const lines = message.split('\n');
+    const headerParts = lines[0].split(' - ');
+
+    const predicates = [
+      lines.length >= 2,
+      lines[0].startsWith('**') && lines[0].endsWith('**'),
+      headerParts.length === 3,
+    ];
+
+    return !predicates.includes(false);
+  },
 });
