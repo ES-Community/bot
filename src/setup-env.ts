ca72760ec9ab343a8ceb6cadf5af39c06fc56909
Mestery <mestery@pm.me>
2021-08-08 16:12:01 +0200

    chore: move dotenv to start script (#39)
    Refs: https://github.com/ES-Community/bot/pull/38#discussion_r684777122

--- src/setup-env.ts
+++ src/setup-env.ts
@@ -1,8 +1,4 @@
-import * as Dotenv from 'dotenv';
-
 process.env.TZ = 'Europe/Paris';
 
 // @ts-expect-error `@types/node` doesn't define this yet.
 process.setSourceMapsEnabled(true);
-
-Dotenv.config();
