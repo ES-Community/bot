43c314818f2920f839007d668b7976ed80edd41d
Michaël Zasso <targos@protonmail.com>
2021-12-02 07:21:37 +0100

    fix: disable CommitStrip cron (#42)
    Their SSL certificate has expired.

--- src/crons/CommitStrip.ts
+++ src/crons/CommitStrip.ts
@@ -5,7 +5,7 @@ import { decode } from 'html-entities';
 import { Cron, findTextChannelByName } from '../framework';
 
 export default new Cron({
-  enabled: true,
+  enabled: false,
   name: 'CommitStrip',
   description:
     'Vérifie toutes les 30 minutes si un nouveau CommitStrip est sorti et le poste dans #gif',
