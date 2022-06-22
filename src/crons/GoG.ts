182902fec4aebd496330ffb20b415fd22eafb870
Purexo <contact@purexo.mom>
2022-06-22 21:43:40 +0200

    test and add more info

--- src/crons/GoG.ts
+++ src/crons/GoG.ts
@@ -1,73 +1,65 @@
-import { MessageEmbed } from 'discord.js';
-import got from 'got';
-import { Logger } from 'pino';
+import { MessageEmbed } from "discord.js";
+import got from "got";
+import { Logger } from "pino";
 
-import { Cron, findTextChannelByName } from '../framework';
-import {parse} from "node-html-parser";
+import { Cron, findTextChannelByName } from "../framework";
+import { parse } from "node-html-parser";
 
 const dateFmtOptions: Intl.DateTimeFormatOptions = {
-  timeZone: 'Europe/Paris',
-  year: 'numeric',
-  month: 'long',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
+  timeZone: "Europe/Paris",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric"
 };
 
 export default new Cron({
   enabled: true,
-  name: 'GoG',
+  name: "GoG",
   description:
-    'Vérifie tous les jours à 17h30 (Paris) si GoG offre un jeu (promotion gratuite) et alerte dans #jeux',
+    "Vérifie tous les jours à 17h30 (Paris) si GoG offre un jeu (promotion gratuite) et alerte dans #jeux",
   schedule: '30 17 * * *',
+  // schedule: "* * * * *", // switch for testing
   async handle(context) {
     const game = await getOfferedGame(context.logger);
     if (!game) {
       return;
     }
-
-    const channel = findTextChannelByName(context.client.channels, 'jeux');
     
-    context.logger.info(`Found a new offered game (${game.title})`);
-  
+    const channel = findTextChannelByName(context.client.channels, "jeux");
+    
     await channel.send(
-      new MessageEmbed({ title: game.title, url: game.link })
+      new MessageEmbed()
+        .setTitle(game.title)
+        .setURL(game.link)
+        .setDescription(game.description)
         .setThumbnail(game.thumbnail)
+        .setImage(game.banner)
         .addField(
-          'Fin',
-          game.discountEndDate.toLocaleDateString('fr-FR', dateFmtOptions),
-          true,
+          "Fin",
+          game.discountEndDate.toLocaleDateString("fr-FR", dateFmtOptions),
+          true
         )
-        .addField('Prix', `${game.originalPrice} → **Gratuit**`)
-        .setTimestamp(),
+        .addField("Prix", `${game.originalPrice} → **Gratuit**`)
+        .addField("Note", `⭐ ${game.rating}`)
+        .setTimestamp()
     );
-  },
+  }
 });
 
 /**
- * Game information extracted from the Epic Games GraphQL API.
+ * Game information extracted from the GoG Game (prerendered HTML scrapping).
  */
 interface Game {
-  /**
-   * Game title.
-   */
   title: string;
-  /**
-   * Game link.
-   */
+  description: string;
   link: string;
-  /**
-   * Game thumbnail.
-   */
   thumbnail: string;
-  /**
-   * Game formatted original price.
-   */
+  banner: string;
   originalPrice: string;
-  /**
-   * Game discount end date.
-   */
   discountEndDate: Date;
+  rating: string;
 }
 
 /**
@@ -76,44 +68,51 @@ interface Game {
  * @param logger
  */
 async function getOfferedGame(logger: Logger): Promise<Game | null> {
-  const { body: homeBody } = await got<string>('https://www.gog.com/#giveaway', {
+  const GOG_GOT_OPTIONS = {
     headers: {
-      'Accept-Language': 'fr,fr-FR;q=0.8,fr-FR;q=0.7,en-US;q=0.5,en-US;q=0.3,en;q=0.2',
-    },
-  });
+      "Accept-Language": "fr,fr-FR;q=0.8,fr-FR;q=0.7,en-US;q=0.5,en-US;q=0.3,en;q=0.2"
+    }
+  };
+  const { body: homeBody } = await got<string>("https://www.gog.com/#giveaway", GOG_GOT_OPTIONS);
   
   if (!homeBody) return null;
   const html = parse(homeBody);
   if (!html) return null;
-  const SEOLink = html.querySelector('a#giveaway');
+  const SEOLink = html.querySelector("a#giveaway");
   if (!SEOLink) return null;
   const endTimestamp = Number(
-    SEOLink
-      .querySelector('gog-countdown-timer')
-      ?.getAttribute('end-date')
+    html
+      .querySelector(".giveaway-banner__footer gog-countdown-timer")
+      ?.getAttribute("end-date")
   );
   
-  const { body: gameBody } = await got<string>(`https://www.gog.com${SEOLink.getAttribute('ng-href')}`, {
-    headers: {
-      'Accept-Language': 'fr,fr-FR;q=0.8,fr-FR;q=0.7,en-US;q=0.5,en-US;q=0.3,en;q=0.2',
-    },
-  });
+  const { body: gameBody } = await got<string>(
+    `https://www.gog.com${SEOLink.getAttribute("ng-href")}`,
+    GOG_GOT_OPTIONS
+  );
   if (!gameBody) return null;
   const gameHTML = parse(gameBody);
   if (!gameHTML) return null;
   
-  const ldJSONNode = gameHTML.querySelector('script[type="application/ld+json"]');
-  const gameJSON = JSON.parse(ldJSONNode?.innerHTML ?? '');
+  const ldJSONNode = gameHTML.querySelector("script[type=\"application/ld+json\"]");
+  const gameJSON = JSON.parse(ldJSONNode?.innerHTML ?? "");
   if (!gameJSON) return null;
   
-  logger.info({ data: gameJSON }, 'Offered games response');
+  const description = gameHTML.querySelector(".content-summary-item__description")?.innerText ?? "";
+  const banner = gameHTML.querySelector("head meta[property=\"og:image\"]")?.getAttribute("content") ?? "";
+  const rating = gameHTML.querySelector(".rating.productcard-rating__score")?.innerText ?? "";
+  
+  logger.info({ data: { gameJSON, description, banner } }, "Offered games response");
   
   return {
     title: gameJSON.name,
+    description,
     // link: 'https://www.gog.com/#giveaway',
     link: gameJSON.offers.url,
-    thumbnail: gameJSON.image ?? '',
+    thumbnail: gameJSON.image,
+    banner,
     originalPrice: gameJSON.offers.price,
     discountEndDate: new Date(endTimestamp),
+    rating
   };
 }
