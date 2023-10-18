7ba4830e419e2f5a8ec553c395cf22ecf5e9dd66
Michaël Zasso <targos@protonmail.com>
2023-10-18 09:22:46 +0200

    chore: update Knex to v3 and fix import for Vitest

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,8 +1,10 @@
-import knex, { Knex } from 'knex';
+import type { Knex } from 'knex';
+// @ts-expect-error Import directly from the mjs file because knex exports aren't configured correctly.
+import { knex } from 'knex/knex.mjs';
 import Environments from '../../knexfile.js';
 
 export const env = process.env.NODE_ENV || 'development';
 export const config: Knex.Config = Environments[env];
 
-export const DB = knex.knex(config);
+export const DB: Knex = knex(config);
 export default DB;
