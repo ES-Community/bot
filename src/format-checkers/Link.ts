594e8fc2faf65d8fd661ce4e9d07ee780d38410d
Purexo <5005154+Purexo@users.noreply.github.com>
2024-05-17 11:49:45 +0200

    fix: support emoji modifier

--- src/format-checkers/Link.ts
+++ src/format-checkers/Link.ts
@@ -2,7 +2,7 @@ import { FormatChecker } from '../framework/index.js';
 
 const urlRegexp =
   '<?https?:\\/\\/(?:www\\.)?[-\\w@:%.\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-\\w()@:%\\+.~#?&//=]*)>?';
-const titleRegexp = `(?:[a-zA-Z0-9_\\- ]|(?:<a?:\\w{2,32}:\\d{17,20}>)|(?::\\w{2,32}:)|\\p{Emoji_Presentation})+`;
+const titleRegexp = `(?:[a-zA-Z0-9_\\- ]|(?:<a?:\\w{2,32}:\\d{17,20}>)|(?::\\w{2,32}:)|(?:\\p{Emoji_Presentation}\\p{Emoji_Modifier}*))+`;
 const linkRegexp = `^\\[( )?(\\*\\*)?${titleRegexp}\\2\\1\\](?:[^\\n])+ - ${urlRegexp}$`;
 
 export default new FormatChecker({
--- src/format-checkers/Project.ts
+++ src/format-checkers/Project.ts
@@ -2,7 +2,7 @@ import { FormatChecker } from '../framework/index.js';
 
 const urlRegexp =
   '<?https?:\\/\\/(?:www\\.)?[-\\w@:%.\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b(?:[-\\w()@:%\\+.~#?&//=]*)>?';
-const headerRegexp = `(?:[a-zA-Z0-9_\\- ]|(?:<a?:\\w{2,32}:\\d{17,20}>)|(?::\\w{2,32}:)|\\p{Emoji_Presentation})+`;
+const headerRegexp = `(?:[a-zA-Z0-9_\\- ]|(?:<a?:\\w{2,32}:\\d{17,20}>)|(?::\\w{2,32}:)|(?:\\p{Emoji_Presentation}\\p{Emoji_Modifier}*))+`;
 const projectRegexp = new RegExp(
   `^\\*\\*${headerRegexp}+\\*\\*\\n\\n(?:.*\\n)+\n(?:(?:${headerRegexp}* )?${urlRegexp}\n)+$`,
   'u',
